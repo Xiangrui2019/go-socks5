2b69b188e740c1392969eb152835f530b5da53ae
Xiangrui2019 <xiangrui@aiursoft.com>
2019-07-03 09:50:12 +0800

    Change config file name

--- config.go
+++ config.go
@@ -24,7 +24,7 @@ type Config struct {
 func init() {
 	home, _ := homedir.Dir()
 	// 默认的配置文件名称
-	configFilename := ".lightsocks.json"
+	configFilename := ".gosocks5.json"
 	// 如果用户有传配置文件，就使用用户传入的配置文件
 	if len(os.Args) == 2 {
 		configFilename = os.Args[1]
